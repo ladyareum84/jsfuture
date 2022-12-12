e5b9b018b65e9046253f1415fbc3c9ad83603aab
design <ladyareum.office@gmail.com>
2022-12-12 17:31:01 +0900

    jsf workspce update

---
+++ clipdown/2022/web/m_top/resource/js/common.js
@@ -0,0 +1,85 @@
+// 새로고침 시 scrollTop(강제) 
+$(window).on('beforeunload', function() {
+    $(window).scrollTop(0); 
+});
+
+//jQuery to collapse the navbar on scroll
+var header_height  = $('.navbar').height(),
+    intro_height    = $('.intro-section').height(),
+    offset_val = intro_height + header_height;
+$(window).scroll(function() {
+  var scroll_top = $(window).scrollTop();
+    if (scroll_top >= offset_val) {
+        $(".navbar-fixed-top").addClass("top-nav-collapse");
+            $(".navbar-fixed-top").removeClass("navbar-transparent");
+    } else {
+        $(".navbar-fixed-top").removeClass("top-nav-collapse");
+      $(".navbar-fixed-top").addClass("navbar-transparent");
+    }
+});
+
+//jQuery for page scrolling feature - requires jQuery Easing plugin
+$(function() {
+    $('a.page-scroll').bind('click', function(event) {
+        var $anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: $($anchor.attr('href')).offset().top
+        }, 1500, 'easeInOutExpo');
+        event.preventDefault();
+    });
+});
+
+
+// layerPopup
+function popupOpenClose(popup) {
+	
+	/* Add div inside popup for layout if one doesn't exist */
+	if ($(".wrapper").length == 0){
+		$(popup).wrapInner("<div class='wrapper'></div>");
+	}
+	
+	/* Open popup */
+	$(popup).show();
+
+	/* Close popup if user clicks on background */
+	$(popup).click(function(e) {
+		if ( e.target == this ) {
+			if ($(popup).is(':visible')) {
+				$(popup).hide();
+			}
+		}
+	});
+
+	/* Close popup and remove errors if user clicks on cancel or close buttons */
+	$(popup).find("button[name=close]").on("click", function() {
+		if ($(".formElementError").is(':visible')) {
+			$(".formElementError").remove();
+		}
+		$(popup).hide();
+	});
+}
+
+$(document).ready(function () {
+	$("[data-js=open]").on("click", function() {
+		popupOpenClose($(".popup"));
+	});
+});
+
+
+// Scroll to Top
+jQuery(document).ready(function () {
+    $(window).scroll(function () {
+      if ($(this).scrollTop() > 600) {
+        $('.back-to-top').fadeIn(500);
+      } else {
+        $('.back-to-top').fadeOut(300);
+      }
+    });
+    $('.back-to-top').click(function (e) {
+      e.preventDefault();
+      $('html, body').animate({scrollTop: 0}, 500);
+    });
+});
+
+
+
