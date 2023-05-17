46b6f8e6a252919ce492524084143fb79e5c2e20
design <ladyareum.office@gmail.com>
2023-05-17 14:26:42 +0900

    cashmate style update

--- clipdown/2023/webview/cashmate/resource/js/common_cashmate.js
+++ clipdown/2023/webview/cashmate/resource/js/common_cashmate.js
@@ -1,6 +1,6 @@
 //Tabs
-$('.tab-container .tab-content').not(':first').hide();
-$('.tab-container .sub-content').each(function(){
+$('.tab_container .tab_content').not(':first').hide();
+$('.tab_container .sub_content').each(function(){
 	var idValue = $(this).attr('id');
 	if(idValue.charAt(idValue.length - 1) != 1){
 		$(this).hide();
@@ -9,28 +9,28 @@ $('.tab-container .sub-content').each(function(){
 	}
 });
 
-$('.tabs.tabs-nav li').click(function(){
-	$('.tabs.tabs-nav li').removeClass('active'); 
+$('.tabs.tabs_nav li').click(function(){
+	$('.tabs.tabs_nav li').removeClass('active'); 
 	$(this).addClass('active');					 
 	var tab_id = $(this).attr('rel');
-	$('.tab-container .tab-content').hide();
-	$('#'+tab_id).show();
-
-});
-$('.tabs.table-tabs-nav li').click(function(){
-	$('.tabs.table-tabs-nav li').removeClass('active'); 
-	$(this).addClass('active');					 
-	var tab_id = $(this).attr('rel');
-	$('.tab-container .tab-content').hide();
+	$('.tab_container .tab_content').hide();
 	$('#'+tab_id).show();
 });
 
-// MultiTabs - 2depth일때사용
-$('.tabs.sub-tabs-nav li').click(function(){
+$('.tabs.table_tabs_nav li').click(function(){
+	$('.tabs.table_tabs_nav li').removeClass('active'); 
+	$(this).addClass('active');					 
+	var tab_id = $(this).attr('rel');
+	$('.tab_container .tab_content').hide();
+	$('#'+tab_id).show();
+});
+
+// MultiTabs _ 2depth일때사용
+$('.tabs.sub_tabs_nav li').click(function(){
 	$(this).parent().children().removeClass('active'); 
 	$(this).addClass('active');
 	var tab_id = $(this).attr('rel');
-	$('.tab-container .sub-content').each(function(){
+	$('.tab_container .sub_content').each(function(){
 		if($(this).attr('id').substring(0, 6) == tab_id.substring(0, 6)){
 			$(this).hide();
 		}
