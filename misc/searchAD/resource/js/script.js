e723e193b6562d839d88617ce3e22677afa789af
design <ladyareum.office@gmail.com>
2023-12-14 14:18:09 +0900

    update

--- misc/searchAD/resource/js/script.js
+++ misc/searchAD/resource/js/script.js
@@ -1,3 +1,13 @@
+// 모바일 웹 100vh 세로 사이즈 문제해결
+function setScreenSize() {
+	let vh = window.innerHeight * 0.01;
+	document.documentElement.style.setProperty('--vh', `${vh}px`);
+}
+setScreenSize();
+window.addEventListener('resize', setScreenSize);
+
+
+
 gsap.registerPlugin(ScrollTrigger);
 
 ScrollTrigger.defaults({
