cbd7ab4323084fab4ab0b25eb9fbe7ccc8137c9e
design <ladyareum.office@gmail.com>
2023-12-18 19:16:54 +0900

    update

--- misc/searchAD/resource/js/script.js
+++ misc/searchAD/resource/js/script.js
@@ -1,6 +1,6 @@
-function setScreenSize() {
-	let vh = window.innerHeight * 0.01;
-	document.documentElement.style.setProperty('--vh', `${vh}px`);
-}
-setScreenSize();
-window.addEventListener('resize', setScreenSize);
+// function setScreenSize() {
+// 	let vh = window.innerHeight * 0.01;
+// 	document.documentElement.style.setProperty('--vh', `${vh}px`);
+// }
+// setScreenSize();
+// window.addEventListener('resize', setScreenSize);
